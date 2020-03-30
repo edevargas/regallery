cdef79a850f8b3c99ce702288db92f8fa4093c86
Elías <sailedvc@gmail.com>
2020-03-29 19:47:07 -0500

    8.6 Ajuste de header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -4,8 +4,8 @@ export const SHeaderContainer = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
-  height: 70px;
+  height: 75px;
 `
 export const SLogoWrapper = styled.div`
-  width: 220px;
+  height: 100%;
 `
